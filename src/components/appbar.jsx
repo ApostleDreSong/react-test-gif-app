32fd079f8f958ad594850d883a3a61aacdccb0b7
Damilare Ademeso <ademesodamilare@gmail.com>
2020-01-16 12:38:09 +0100

    addition

--- src/components/appbar.jsx
+++ src/components/appbar.jsx
@@ -8,7 +8,6 @@ import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
 import TextField from '@material-ui/core/TextField';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
