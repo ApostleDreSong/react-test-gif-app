5314bcfadd58562c76fb35bdd1491928a89ac67b
Damilare Ademeso <ademesodamilare@gmail.com>
2020-01-17 18:13:29 +0100

    merged

--- src/store/index.js
+++ src/store/index.js
@@ -13,13 +13,8 @@ import thunk from 'redux-thunk';
 
 const middleware = [thunk];
 
-const store = createStore(
-	rootReducer,
-	compose(
-		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
 
 //let persistor = persistStore(store);
 
