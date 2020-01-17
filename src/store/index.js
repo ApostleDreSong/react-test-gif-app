3cf68c42cefc85690398e9e62d216a9c7bdd943b
Damilare Ademeso <ademesodamilare@gmail.com>
2020-01-17 12:23:38 +0100

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -13,12 +13,8 @@ import thunk from 'redux-thunk';
 
 const middleware = [thunk];
 
-const store = createStore(
-	rootReducer,
-	compose(
-		applyMiddleware(...middleware)
-	)
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
 
 //let persistor = persistStore(store);
 
